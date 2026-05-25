0bc9d54ad55dd417bc69e0c0c22aedb160ed2fc4
nathanbish <nathan.bish@colorado.edu>
2026-05-25 19:09:48 +0100

    Update list.js

--- pages/api/email/list.js
+++ pages/api/email/list.js
@@ -2,7 +2,6 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '../auth/[...nextauth]'
 import { listEmails } from '../../../lib/gmail'
 import { listOutlookEmails } from '../../../lib/outlook'
-import { prioritizeEmails } from '../../../lib/ai'
 
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions)
@@ -10,18 +9,12 @@ export default async function handler(req, res) {
 
   try {
     let emails = []
-
     if (session.provider === 'google') {
       emails = await listEmails(session.accessToken, { maxResults: 100 })
     } else if (session.provider === 'azure-ad') {
-      emails = await listOutlookEmails(session.accessToken, { top: 25 })
+      emails = await listOutlookEmails(session.accessToken, { top: 100 })
     }
-
-    const prioritized = req.query.prioritize === 'true'
-      ? await prioritizeEmails(emails)
-      : null
-
-    res.json({ emails, prioritized })
+    res.json({ emails })
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: err.message })
